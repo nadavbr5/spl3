764ec3b669c8289679dff816b8a65be21bb06a4c
nadavbr5 <nadavbr5@gmail.com>
2017-12-31 14:06:47 -0500

    init

---
+++ src/bgu/spl181/net/srv/TcpServer.java
@@ -0,0 +1,64 @@
+package bgu.spl181.net.srv;
+
+import bgu.spl181.net.api.MessageEncoderDecoder;
+import bgu.spl181.net.api.MessagingProtocol;
+import bgu.spl181.net.srv.bidi.BlockingConnectionHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.function.Supplier;
+
+public class TcpServer<T> implements Server<T> {
+
+    private final int port;
+    private final Supplier<MessagingProtocol<T>> protocolFactory;
+    private final Supplier<MessageEncoderDecoder<T>> encdecFactory;
+    private ServerSocket sock;
+
+    public TcpServer(
+            int port,
+            Supplier<MessagingProtocol<T>> protocolFactory,
+            Supplier<MessageEncoderDecoder<T>> encdecFactory) {
+
+        this.port = port;
+        this.protocolFactory = protocolFactory;
+        this.encdecFactory = encdecFactory;
+		this.sock = null;
+    }
+
+    @Override
+    public void serve() {
+
+        try (ServerSocket serverSock = new ServerSocket(port)) {
+			System.out.println("Server started");
+
+            this.sock = serverSock; //just to be able to close
+
+            while (!Thread.currentThread().isInterrupted()) {
+
+                Socket clientSock = serverSock.accept();
+
+                BlockingConnectionHandler<T> handler = new BlockingConnectionHandler<>(
+                        clientSock,
+                        encdecFactory.get(),
+                        protocolFactory.get());
+
+                execute(handler);
+            }
+        } catch (IOException ex) {
+        }
+
+        System.out.println("server closed!!!");
+    }
+
+    @Override
+    public void close() throws IOException {
+		if (sock != null)
+			sock.close();
+    }
+
+    private void execute(BlockingConnectionHandler<T> handler) {
+        new Thread(handler).start();
+    }
+}
