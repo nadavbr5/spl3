d77f483567bedb5b2a080de0a2154d6243bae981
shakedtu <shakit@me.com>
2018-01-01 19:50:21 +0200

    s

---
+++ Server/src/bgu/spl181/net/api/bidi/ConnectionsTPC.java
@@ -0,0 +1,39 @@
+package bgu.spl181.net.api.bidi;
+
+
+import bgu.spl181.net.srv.Server;
+import bgu.spl181.net.srv.bidi.BlockingConnectionHandler;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ConnectionsTPC<String> implements Connections<String> {
+
+    private ConcurrentHashMap<Integer, BlockingConnectionHandler<String>> map = new ConcurrentHashMap<>();
+
+    @Override
+    public boolean send(int connectionId, String msg) {
+        return map.containsKey(connectionId) && map.get(connectionId).send(msg);
+    }
+
+    @Override
+    public boolean broadcast(String msg) {
+        AtomicBoolean sentToAll = new AtomicBoolean(true);
+        map.values().forEach((connection) -> sentToAll.compareAndSet(true, connection.send(msg)));
+        return sentToAll.get();
+    }
+
+    @Override
+    public void disconnect(int connectionId) {
+        BlockingConnectionHandler handler = map.remove(connectionId);
+        if (handler != null) {
+            try {
+                handler.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
