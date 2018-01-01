cccb3b487e35306b2282479532044b0a312966ea
shakedtu <shakit@me.com>
2018-01-01 18:45:16 +0200

    s

--- src/bgu/spl181/net/Movie.java
+++ src/bgu/spl181/net/Movie.java
@@ -1,41 +1,59 @@
 package bgu.spl181.net;
 
+import java.util.ArrayList;
+
 public class Movie {
     private int id;
     private String name;
     private double price;
-    private String[] bannedCountries;
+    private ArrayList<String> bannedCountries;
     private int availableAmount;
     private int totalAmount;
 
     //TODO:: check if the name already exists in the system and if the price or the amount are <=0
-    public Movie(int id, String name, double price, String[] bannedCountries, int availableAmount, int totalAmount){
-        this.id=id;
-        this.name=name;
-        this.price=price;
-        this.bannedCountries=bannedCountries;
-        this.availableAmount=availableAmount;
-        this.totalAmount=totalAmount;
+    public Movie(int id, String name, double price, ArrayList<String> bannedCountries, int availableAmount, int totalAmount) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.bannedCountries = bannedCountries;
+        this.availableAmount = availableAmount;
+        this.totalAmount = totalAmount;
     }
 
-    public void setPrice(double newPrice){
-        this.price=newPrice;
+    public void setPrice(double newPrice) {
+        this.price = newPrice;
     }
 
-    public boolean rent(){
-        if(availableAmount == 0)
+    //returns false if the movie is not available, otherwise decrease availableAmount and returns true
+    public boolean rentMovie() {
+        if (availableAmount == 0) return false;
+        this.availableAmount--;
+        return true;
+    }
+
+    public boolean isbannedCountry(String country) {
+        return this.bannedCountries.contains(country);
+    }
+
+    public boolean rent() {
+        if (availableAmount == 0)
             return false;
         this.availableAmount--;
         return true;
     }
 
-    public void returnMovie(){
+    public void returnMovie() {
         this.availableAmount++;
     }
 
+    //return true if there is at least one user renting this movie
+    public boolean isRented() {
+        return availableAmount != totalAmount;
+    }
+
     //TODO:: change the function
-    public String toString(){
-        return "\"name\" "+ availableAmount + " " + totalAmount + " " + bannedCountries.toString();
+    public String toString() {
+        return "\"name\" " + availableAmount + " " + totalAmount + " " + bannedCountries.toString();
 
     }
 }
--- src/bgu/spl181/net/srv/User.java
+++ src/bgu/spl181/net/srv/User.java
@@ -1,21 +1,48 @@
 package bgu.spl181.net.srv;
 
+import java.util.ArrayList;
+
 public class User {
 
     private String name;
     private String type;
     private String password;
-    private String [] movies;
+    private ArrayList<String> movies;
     private int balance;
 
-    //TODO:: check that the inputs are valid
-    public User(String name, String type, String password, String[] movies, int balance) {
+    //TODO:: check that the inputs are valid- if the name is not already exits in the system ,type is admin/ normal, balance is a number >=0
+    public User(String name, String type, String password, ArrayList<String> movies, int balance) {
         this.name = name;
         this.type = type;
         this.password = password;
         this.movies = movies;
         this.balance = balance;
     }
+
+    public boolean isAdmin() {
+        return this.type.equals("admin");
+    }
+
+    //we can assume that the number is greater than 0
+    public void increaseBalance(int newBalance){
+        this.balance+=newBalance;
+    }
+
+    //TODO:: check if newBalance is valid
+    public boolean reduceBalance(int newBalance){
+        int nBalance= this.balance-newBalance;
+        if(nBalance >= 0){
+            this.balance=nBalance;
+            return true;
+        }
+        return false;
+    }
+
+    //returns true if this user already rent the input movie
+    public boolean alreadyRent(String movieName){
+        return this.movies.contains(movieName);
+    }
+
 
 
 
