ff68a4574b00ea66ce7f6f19fd17309b85ae57ab
nogakl <nogakl@post.bgu.ac.il>
2018-01-04 17:09:22 +0200

    sg

---
+++ Client/include/Task.h
@@ -0,0 +1,29 @@
+//
+// Created by nogakl on 1/4/18.
+//
+
+#ifndef CLIENT_TASK_H
+#define CLIENT_TASK_H
+
+#include <string>
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include "connectionHandler.h"
+
+using boost::asio::ip::tcp;
+
+
+class Task {
+private:
+    boost::mutex *mutex;
+    ConnectionHandler *connectionHandler;
+    std::atomic<bool> *shouldTerminate;
+public:
+    Task(boost::mutex *mutex, ConnectionHandler *connectionHandler, std::atomic<bool> *pAtomic);
+
+    void run();
+};
+
+
+#endif //CLIENT_TASK_H
---
+++ Client/src/Task.cpp
@@ -0,0 +1,38 @@
+//
+// Created by nogakl on 1/4/18.
+//
+
+#include "../include/Task.h"
+
+Task::Task(boost::mutex *mutex, ConnectionHandler *connectionHandler, std::atomic<bool> *pAtomic)
+        : mutex(mutex), connectionHandler(connectionHandler),shouldTerminate(pAtomic) {
+}
+
+void Task::run() {
+    while (!shouldTerminate->load()) {
+        // We can use one of three options to read data from the server:
+        // 1. Read a fixed number of characters
+        // 2. Read a line (up to the newline character using the getline() buffered reader
+        // 3. Read up to the null character
+        std::string answer;
+        // Get back an answer: by using the expected number of bytes (len bytes + newline delimiter)
+        // We could also use: connectionHandler.getline(answer) and then get the answer without the newline char at the end
+        if (!connectionHandler->getLine(answer)) {
+            std::cout << "Disconnected. Exiting...\n" << std::endl;
+            return;
+        }
+
+        int len = answer.length();
+        // A C string must end with a 0 char delimiter.  When we filled the answer buffer from the socket
+        // we filled up to the \n char - we must make sure now that a 0 char is also present. So we truncate last character.
+        answer.resize(len - 1);
+
+            std::cout << "< " << answer << " " << len << " bytes " << std::endl << std::endl;
+            if (answer == "ACK signout succeeded") {
+                std::cout << "Exiting...\n" << std::endl;
+                shouldTerminate->store(true);
+                return;
+            }
+    }
+
+}
